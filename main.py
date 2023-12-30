43531ba71576a674cd312c824aff5e41fe4fa218
ishanp <eshanpurekar@gmail.com>
2023-12-31 01:02:31 +0900

    Bowler season rec added

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def page_4():
             if bats_rec3:
                 return render_template('Batsman.html', batter_rec=bats_rec1, against_rec=bats_rec2, pom_rec=bats_rec3, batter_names=batter_name, batsman1=batsman1)
             else:
-                return render_template('Batsman.html', batter_rec=bats_rec1, against_rec=bats_rec2, batter_names=batter_name, batsman1=batsman1, pmessage='No Player of Match Record to Show')
+                return render_template('Batsman.html', batter_rec=bats_rec1, against_rec=bats_rec2, batter_names=batter_name, batsman1=batsman1, pmessage='No Record to Show')
         else:
             return render_template('Batsman.html', batter_names=batter_name, message='No Record To Show', batsman1=batsman1)
     else:
@@ -90,6 +90,33 @@ def page_7():
     else:
         return render_template('playerofmatch.html', player_name = player_names, message='Please Select a Player')
 
+@website.route('/all_bowlers')
+def page_9():
+    all_names = requests.get('http://127.0.0.1:5002/api/bowlers')
+    bowler_names = all_names.json()['Bowlers']
+    return render_template('Bowlers.html', bowler_name=bowler_names)
+
+
+@website.route('/bowler_record', methods=['get'])
+def page_10():
+    all_names = requests.get('http://127.0.0.1:5002/api/bowlers')
+    bowler_names = all_names.json()['Bowlers']
+
+    bowler1 = request.args.get('bowler_name')
+    if bowler1:
+        season_record = requests.get(f'http://127.0.0.1:5002/api/bowling_rec?bowler={bowler1}')
+        bowler_rec1 = season_record.json()["Bowler Record by Season"]
+
+        pom_record = requests.get(f'http://127.0.0.1:5002/api/POM_record?pom={bowler1}')
+        bats_rec3 = pom_record.json()["Player of Match Record"]
+
+        if bowler_rec1:# or bowler_rec2:
+            if bats_rec3:
+                return render_template('Bowlers.html', season_rec=bowler_rec1, pom_rec=bats_rec3, bowler_name=bowler_names, bowler1=bowler1)
+            else:
+                return render_template('Bowlers.html', season_rec=bowler_rec1,bowler_name=bowler_names, bowler1=bowler1, pmessage='No Record to Show')
+    else:
+        return render_template('Bowlers.html', bowler_name=bowler_names, message='Please Select a Bowler')
 
 
 if __name__ == '__main__':
